f7d1b283cdcb7a17f19a65bb61951936b017c308
Verbanderbog <33205555+Verbanderbog@users.noreply.github.com>
2023-11-09 14:54:15 -0500

    Step 7
    Creating a Rat Enemy

---
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var sword_ability: PackedScene
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.timeout.connect(on_timer_timeout)
+
+func on_timer_timeout():
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
