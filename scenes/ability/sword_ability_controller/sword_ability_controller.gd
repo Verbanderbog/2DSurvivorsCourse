20a25507501acf6fd2950376220a7ed2bc35c8ac
Verbanderbog <33205555+Verbanderbog@users.noreply.github.com>
2023-11-09 19:23:33 -0500

    step 10

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,17 +1,29 @@
 extends Node
 
+const MAX_RANGE_SQUARED = pow(150,2)
+
 @export var sword_ability: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.timeout.connect(on_timer_timeout)
 
 func on_timer_timeout():
+	var enemies = get_tree().get_nodes_in_group("enemy")
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
+	enemies = enemies.filter(func (enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) < MAX_RANGE_SQUARED
+	)
+	if enemies.size() == 0:
+		return
+	enemies.sort_custom(func(a:Node2D, b:Node2D):
+		return a.global_position.distance_squared_to(player.global_position) < b.global_position.distance_squared_to(player.global_position)
+	)
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player.global_position
+	sword_instance.global_position = enemies[0].global_position
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
