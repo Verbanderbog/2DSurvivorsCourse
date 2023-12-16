585ab4a19081bd6d444ba022f5e0e45aea960176
Verbanderbog <33205555+Verbanderbog@users.noreply.github.com>
2023-12-15 21:13:41 -0500

    Step 19

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -3,6 +3,8 @@ extends Node
 const MAX_RANGE_SQUARED = pow(150,2)
 
 @export var sword_ability: PackedScene
+var damage = 5
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.timeout.connect(on_timer_timeout)
@@ -20,8 +22,9 @@ func on_timer_timeout():
 	enemies.sort_custom(func(a:Node2D, b:Node2D):
 		return a.global_position.distance_squared_to(player.global_position) < b.global_position.distance_squared_to(player.global_position)
 	)
-	var sword_instance = sword_ability.instantiate() as Node2D
+	var sword_instance = sword_ability.instantiate() as SwordAbility
 	player.get_parent().add_child(sword_instance)
+	sword_instance.hitbox_component.damage = damage
 	sword_instance.global_position = enemies[0].global_position
 	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0,TAU)) * 4
 	var enemy_direction = enemies[0].global_position - sword_instance.global_position
